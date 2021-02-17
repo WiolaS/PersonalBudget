d0a92665b0e6a6d85346b7274a0e1d8f90e13f49
Wioleta Sobczyk <wioleta.sobczyk5@gmail.com>
2021-02-17 17:17:29 +0100

    add the 'TextFile' class

---
+++ TextFile.h
@@ -0,0 +1,22 @@
+#ifndef TEXTFILE_H
+#define TEXTFILE_H
+#include <iostream>
+#include "User.h"
+#include "AuxiliaryMethods.h"
+#include "Markup.h"
+
+using namespace std;
+
+class TextFile {
+    const string FILE_NAME;
+
+public:
+    TextFile(string fileName) : FILE_NAME(fileName) {
+    }
+    string getFileName();
+    bool checkThatTheFileIsEmpty(fstream &textFile);
+};
+
+#endif // TEXTFILE
+
+
