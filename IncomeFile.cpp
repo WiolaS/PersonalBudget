4793c92adfbdca7a9c3c19ac01c2e0e286d1e559
Wioleta Sobczyk <wioleta.sobczyk5@gmail.com>
2021-02-21 23:40:47 +0100

    add the 'IncomeFile' class

---
+++ IncomeFile.cpp
@@ -0,0 +1,91 @@
+#include "IncomeFile.h"
+#include "AuxiliaryMethods.h"
+#include <windows.h>
+#include <fstream>
+
+using namespace std;
+
+void IncomeFile::setNumberOfLoadedIncomes (int numberOfLoadedIncomes) {
+    idOfTheLastIncome = numberOfLoadedIncomes;
+}
+
+int IncomeFile::getNumberOfLoadedIncomes() {
+    return idOfTheLastIncome;
+}
+
+void IncomeFile::addIncomeToFile(CashFlow income) {
+    int numberOfIncomes = getNumberOfLoadedIncomes();
+
+    string strNumberOfIncomes = AuxiliaryMethods::convertIntToString(numberOfIncomes);
+    int numberOfParameters = 5;
+
+    CMarkup xml;
+
+    if (numberOfIncomes == 0) {
+        xml.AddElem("incomes");
+        xml.IntoElem();
+        xml.AddElem("incomeId", numberOfIncomes + 1);  ///check
+        xml.AddElem( "userId", income.getUserId());
+        xml.AddElem( "date", income.getDate());
+        xml.AddElem( "item", income.getItem());
+        xml.AddElem( "amount", AuxiliaryMethods::convertFloatToStringAndSetPrecisionToTwoDecimalPlaces(income.getAmount()));
+        xml.Save("C:\\Users\\smile\\Desktop\\GitHub_nauka\\PersonalBudget_Wioleta\\incomes.xml");
+        xml.OutOfElem(); // back out level
+
+    } else {
+        bool bSuccess = xml.Load( "C:\\Users\\smile\\Desktop\\GitHub_nauka\\PersonalBudget_Wioleta\\incomes.xml" );
+        xml.ResetPos(); // top of document
+        xml.FindElem(); // users element is root
+        xml.IntoElem(); // inside users
+        while ( xml.FindElem("incomeId") ) {
+            if ( xml.GetData() == strNumberOfIncomes ) {
+                for (int i = 0; i < numberOfParameters; i++) {
+                    xml.FindElem();
+                }
+                xml.AddElem("incomeId", numberOfIncomes + 1);
+                xml.AddElem( "userId", income.getUserId());
+                xml.AddElem( "date", income.getDate());
+                xml.AddElem( "item", income.getItem());
+                xml.AddElem( "amount", AuxiliaryMethods::convertFloatToStringAndSetPrecisionToTwoDecimalPlaces(income.getAmount()));
+
+                xml.Save("C:\\Users\\smile\\Desktop\\GitHub_nauka\\PersonalBudget_Wioleta\\incomes.xml");
+                xml.OutOfElem(); // back out level
+            }
+        }
+    }
+    setNumberOfLoadedIncomes( numberOfIncomes + 1);
+}
+
+
+vector <CashFlow> IncomeFile::loadIncomesOfTheLoggedinUserFromTheFile(int logedInUserId) {
+    CashFlow income;
+    vector <CashFlow> incomes;
+
+    CMarkup xml;
+    bool bSuccess = xml.Load( "C:\\Users\\smile\\Desktop\\GitHub_nauka\\PersonalBudget_Wioleta\\incomes.xml" );
+    xml.ResetPos(); // top of document
+    xml.FindElem(); // users element is root
+    xml.IntoElem(); // inside users
+
+    while ( xml.FindElem("incomeId") ) {
+        setNumberOfLoadedIncomes(AuxiliaryMethods::convertStringToInt(xml.GetData()));
+        income.setCashFlowId(AuxiliaryMethods::convertStringToInt(xml.GetData()));
+
+        xml.FindElem("userId");
+        income.setUserId(AuxiliaryMethods::convertStringToInt(xml.GetData()));
+
+        xml.FindElem("date");
+        income.setDate(xml.GetData());
+
+        xml.FindElem("item");
+        income.setItem(xml.GetData());
+
+        xml.FindElem("amount");
+        income.setAmount(AuxiliaryMethods::convertStringToFloat(xml.GetData()));
+
+        incomes.push_back(income);
+    }
+    return incomes;
+
+}
+
---
+++ IncomeFile.h
@@ -0,0 +1,29 @@
+#ifndef INCOMEFILE_H
+#define INCOMEFILE_H
+#include <iostream>
+#include <vector>
+#include "CashFlow.h"
+#include "TextFile.h"
+#include "Markup.h"
+
+using namespace std;
+
+class IncomeFile : public TextFile {
+
+    int idOfTheLastIncome;
+    //CashFlow income;
+
+
+public:
+    IncomeFile (string incomeFileName): TextFile (incomeFileName)  {
+        idOfTheLastIncome = 0;
+    }
+
+    void addIncomeToFile(CashFlow income);
+    vector <CashFlow> loadIncomesOfTheLoggedinUserFromTheFile(int logedInUserId);
+
+    void setNumberOfLoadedIncomes (int numberOfLoadedIncomes);
+    int getNumberOfLoadedIncomes();
+};
+
+#endif // INCOMEFILE
